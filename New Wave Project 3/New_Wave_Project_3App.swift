14e139942ee63f807238738849ab6c3477ee47dd
Nathan Grunfeld <nategrunfeld@icloud.com>
2022-11-21 07:38:24 -0500

    Initial Commit

---
+++ New Wave Project 3/New_Wave_Project_3App.swift
@@ -0,0 +1,17 @@
+//
+//  New_Wave_Project_3App.swift
+//  New Wave Project 3
+//
+//  Created by Nathan Grunfeld (student LM) on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct New_Wave_Project_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ New Wave Project 3UITests/New_Wave_Project_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  New_Wave_Project_3UITestsLaunchTests.swift
+//  New Wave Project 3UITests
+//
+//  Created by Nathan Grunfeld (student LM) on 11/21/22.
+//
+
+import XCTest
+
+class New_Wave_Project_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
